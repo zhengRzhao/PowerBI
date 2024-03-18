aeac650c4c1ea0d10856d417485fb76c112b30a7
zhengRzhao <109649304+zhengRzhao@users.noreply.github.com>
2024-03-18 22:53:38 +1300

    Delete connect.py
    Delete connect.py does not include SQL

--- connect.py
+++
@@ -1,8 +0,0 @@
-
-
-
-dbuser = "root" #PUT YOUR MySQL username here - usually admin
-dbpass = "ZZ1234567890zz" #"PUT YOUR PASSWORD HERE"
-dbhost = "localhost" #"PUT YOUR AWS Connect String here"
-dbport = "3306"
-dbname = "PGmonitoring"
